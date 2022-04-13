46b07df686ba33f6240c149ea4ed829f95ab7a2d
Rezsek Máté Vencel <mate.vencel.rezsek@kh.hu>
2022-04-13 23:41:26 +0300

    Main dependecy injection

--- app/src/main/java/com/example/beerapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/beerapp/ui/main/MainViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.beerapp.ui.main
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.beerapp.persistance.Beer
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val repository: MainRepository
+) : ViewModel() {
+
+    fun insertBeer() {
+        viewModelScope.launch {
+            repository.insertBeer(
+                Beer(14, "Kőbányai", "finom")
+            )
+        }
+    }
 }
