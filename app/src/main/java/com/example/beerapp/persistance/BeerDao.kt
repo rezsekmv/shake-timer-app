e44afbc381e9687d80d6e2fdb0f3d2b13259c59d
Rezsek Máté Vencel <mate.vencel.rezsek@kh.hu>
2022-04-21 23:17:58 +0300

    create navgraph

--- app/src/main/java/com/example/beerapp/persistance/BeerDao.kt
+++ app/src/main/java/com/example/beerapp/persistance/BeerDao.kt
@@ -2,6 +2,7 @@ package com.example.beerapp.persistance
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
@@ -10,6 +11,6 @@ interface BeerDao {
     @Query("SELECT * FROM beer")
     suspend fun getAll(): List<Beer>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertBeer(beer: Beer)
 }
